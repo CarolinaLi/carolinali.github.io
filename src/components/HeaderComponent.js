90356a1633c2ed55d11aea2384038b285076e941
Carolina <ca2li@edu.uwaterloo.ca>
2019-05-04 16:26:35 -0400

    Added favicon to header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import favicon from './HeaderFavicon.svg';
 import '../css/HeaderComponent.css';
 
 export default class HeaderComponent extends Component {
   render() {
     return (
       <header className="header">
-
+        <img className="favicon" src={favicon} alt="Favicon" />
       </header>
     );
   }
