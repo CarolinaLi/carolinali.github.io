1290e7732e188375a157404f8ac285a1157f5ba6
Carolina <ca2li@edu.uwaterloo.ca>
2019-05-06 11:33:21 -0400

    Updates

---
+++ precache-manifest.1e3723a7e56ecadbc9118dd187d64782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8e6b012412eef3f693162a54a34cc02",
+    "url": "/static/media/HeaderFavicon.a8e6b012.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "29a26291f036310c1911",
+    "url": "/static/js/main.fee2b17f.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "29a26291f036310c1911",
+    "url": "/static/css/main.f002a9f3.chunk.css"
+  },
+  {
+    "revision": "c9b310d9197499c607361ee0ce23545e",
+    "url": "/index.html"
+  }
+];
