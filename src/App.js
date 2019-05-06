2622738eb19342142a05b7cc4dec901532130ce5
Carolina <ca2li@edu.uwaterloo.ca>
2019-05-06 14:42:02 -0400

    Added project links and bootstrap

--- src/App.js
+++ src/App.js
@@ -9,10 +9,23 @@ class App extends Component {
         <Header />
         <div className="block">
           <h1>CAROLINA LI</h1>
-          <p>Hi! I study computer science at uWaterloo and art in my free time.</p>
+          <p>
+            Hi! I study computer science at uWaterloo and art in my free time.
+          </p>
           <p>Welcome to my portfolio.</p>
         </div>
-        <div className="block">
+        <div className="container-fluid block">
+          <div className="row no-gutters">
+            <div className="col-sm-4">
+              <img src={require('./images/green.png')} alt="dev" />
+            </div>
+            <div className="col-sm-4">
+              <img src={require('./images/blue.png')} alt="ux" />
+            </div>
+            <div className="col-sm-4">
+              <img src={require('./images/red.png')} alt="art" />
+            </div>
+          </div>
         </div>
       </div>
     );
