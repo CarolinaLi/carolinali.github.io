8bf4688af245f1f86ddc3b90ee959c678b7105dc
Carolina <ca2li@edu.uwaterloo.ca>
2019-05-05 22:40:47 -0400

    Added navigation links

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,6 +6,10 @@ export default class HeaderComponent extends Component {
   render() {
     return (
       <header className="header">
+        <div className="left-links">
+          <a>Résumé</a>
+          <a>Shop</a>
+        </div>
         <img className="favicon" src={favicon} alt="Favicon" />
       </header>
     );
